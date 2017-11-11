7ecee0f32f82102dc56b24ec479f50269cfa9759
denisrogovoy <denis@knu.ua>
2017-11-11 23:40:08 +0200

    'FirstCommit'

---
+++ ConsoleApplication1.2Lab(1).cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.2Lab(1).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "It's me, your first program.";
+	return 0;
+}
+
+
