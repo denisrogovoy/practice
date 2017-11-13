1c1b7afbfd8890db88b77e23452a42c1f8569426
denisrogovoy <denis@knu.ua>
2017-11-13 12:54:52 +0200

    Lab4

---
+++ ConsoleApplicationLab1.4.40.cpp
@@ -0,0 +1,22 @@
+// ConsoleApplicationLab1.4.40.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(void) {
+	float pi = 3.14159265359;
+	float x, y;
+	cout << "Enter value for x: ";
+	cin >> x;
+	// put your code here
+
+
+	float a, b;
+	a = (x*x) / ((pi*pi)*(x*x + 1. / 2.));
+	b = 1 + ((x*x) / ((pi*pi)*((x*x - 1. / 2.)*(x*x - 1. / 2.))));
+	y = a*b;
+	cout << "y = " << y;
+	return 0;
+}
+
