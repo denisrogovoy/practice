e95c47a2e66eb52e986c1823f143599c28c744b5
denisrogovoy <denis@knu.ua>
2017-11-14 14:54:08 +0200

    Lab10

---
+++ ConsoleApplicationLab2.1.5(1).cpp
@@ -0,0 +1,35 @@
+// ConsoleApplicationLab2.1.5(1).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(void) {
+	int year;
+	cout << "Enter a year: ";
+	cin >> year;
+	// Insert your code here
+	if (year % 4 != 0)
+	{
+		cout << "Common year";
+	}
+	else
+		if (year % 100 != 0)
+		{
+			cout << "Leap year";
+		}
+		else
+		{
+			if (year % 400 != 0)
+			{
+				cout << "Common year";
+			}
+			else
+			{
+				cout << "Leap year";
+			}
+		}
+
+	return 0;
+}
+
