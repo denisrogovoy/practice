7dc909ba42fae3ff5d27b2d3f5d83a6a88dde6f4
denisrogovoy <denis@knu.ua>
2017-11-20 14:05:14 +0200

    Lab12

---
+++ ConsoleApplicationLab2.1.5(3).cpp
@@ -0,0 +1,23 @@
+// ConsoleApplicationLab2.1.5(3).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(void) {
+	int sys;
+	float m, ft, in;
+	// Insert your code here
+	cout << "Please choose: 0-metric, 1-imperial ";
+	cin >> sys;
+	if (sys==0)
+	{
+
+	}
+	else
+	{
+
+	}
+
+	return 0;
+}
