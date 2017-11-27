abd22d3defeef1ee3285c003f0f7fa01d5fcea4d
denisrogovoy <denis@knu.ua>
2017-11-27 19:41:41 +0200

    chapter 2 Lab2.3.19(3) update

---
+++ Chapter 2/2.3 - Labs (9)/ConsoleApplicationLab2.3.19(2).cpp
@@ -0,0 +1,28 @@
+// ConsoleApplication2.3.19(3).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	double pi4 = 0.;
+	long n;
+	cout << "Number of iterations? ";
+	cin >> n;
+
+	int a = 1;
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+			pi4 += 1. / a;
+		else
+			pi4 -= 1. / a;
+		a += 2;
+	}
+
+	cout.precision(20);
+	cout << "Pi = " << (pi4 * 4.) << endl;
+	return 0;
+}
+
