8cf1af87689013c150029a3a2d402b8e3ba4eaf0
denisrogovoy <denis@knu.ua>
2017-11-28 22:34:40 +0200

    chapter2 Lab2.3.19(7)

---
+++ Chapter 2/2.3 - Labs (9)/ConsoleApplicationLab2.3.19(7).cpp
@@ -0,0 +1,32 @@
+// ConsoleApplicationLab2.3.19(7).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int n;
+	cout << "Input number: ";
+	cin >> n;
+
+	long long current = 0;
+	long long previous = 0;
+	long long preprevoius = 0;
+
+	for (int i = 0; i < n; i++) 
+	{
+		if (i == 0)
+			current = 1;
+		else
+		{
+			preprevoius = previous;
+			previous = current;
+			current = preprevoius + previous;
+		}
+	}
+	cout << current << endl;
+	return 0;
+}
