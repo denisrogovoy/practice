e0ebc1d7d263a05e2d6383272a34719808119957
denisrogovoy <denis@knu.ua>
2017-12-06 06:46:42 +0200

    Lab5.1.9(1)+(2)

---
+++ Chapter 5/5.1 - Labs (4)/ConsoleApplicationLab5.1.9(2).cpp
@@ -0,0 +1,56 @@
+// ConsoleApplicationLab5.1.9(2).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Square
+{
+public:
+	Square(double side);
+
+private:
+	double side;
+	double area;
+
+public:
+	void SetSide(double side);
+	void print();
+};
+
+void Square::SetSide(double side) {
+	// ignoring values lower or equal than zero
+	if (side <= 0)
+		return;
+
+	this->side = side;
+	this->area = side * side;
+}
+
+Square::Square(double side)
+{
+	this->side = side;
+	this->area = side * side;
+}
+
+void Square::print()
+{
+	cout << "Square: side=" << this->side << " area=" << this->area << endl;
+}
+
+int main()
+{
+	Square s(4);
+	s.print();
+
+	s.SetSide(2.0);
+	s.print();
+
+	s.SetSide(-33);
+	s.print();
+
+	return 0;
+}
