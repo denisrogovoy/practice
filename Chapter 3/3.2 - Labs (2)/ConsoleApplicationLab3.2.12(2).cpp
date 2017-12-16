dda3de6836decd9a970e9298768e82ba72a62b6c
denisrogovoy <denis@knu.ua>
2017-12-16 22:33:28 +0200

    Lab3.2.12(2)

---
+++ Chapter 3/3.2 - Labs (2)/ConsoleApplicationLab3.2.12(2).cpp
@@ -0,0 +1,27 @@
+// ConsoleApplicationLab3.2.12(2).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int matrix[10][10] = {};
+	int *current = matrix[0];
+	for (int i = 1; i <= 10; i++)
+		for (int j = 1; j <= 10; j++)
+		{
+			*current = j * i;
+			current++;
+		}
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+		{
+			cout.width(4);
+			cout << matrix[i][j];
+		}
+	cout << endl;
+	return 0;
+}
+
