a6afa28f241fa2312a3043fa2553301b475a44af
denisrogovoy <denis@knu.ua>
2017-12-16 22:22:40 +0200

    Lab3.2.12(1)

---
+++ Chapter 3/3.2 - Labs (2)/ConsoleApplicationLab3.2.12(1).cpp
@@ -0,0 +1,23 @@
+// ConsoleApplicationLab3.2.12(1).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int vector[] = { 3, -5, 7, 10, -4, 14, 5, 2, -13 };
+	int n = sizeof(vector) / sizeof(vector[0]);
+	int *currentElement = vector;
+	int minimum = 0;
+	for (int i = 0; i < n; i++)
+		if (minimum > *currentElement) 
+		{
+			minimum = *currentElement;
+			currentElement++;
+		}
+	cout << "Minimum element is " << minimum << endl;
+	return 0;
+}
