8b5b93067e63e68e64839fef8ccb6921a6187f85
denisrogovoy <denis@knu.ua>
2017-12-16 22:46:35 +0200

    Lab3.4.7(1)

---
+++ Chapter 3/3.4 - Labs (6)/ConsoleApplicationLab3.4.7(1).cpp
@@ -0,0 +1,20 @@
+// ConsoleApplicationLab3.4.7(1).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+using namespace std;
+
+bool isLeap(int year)
+{
+	return year % 4 == 0;
+}
+int main(void)
+{
+	for (int yr = 1995; yr < 2017; yr++)
+		cout << yr << " -> " << isLeap(yr) << endl;
+	return 0;
+}
+
