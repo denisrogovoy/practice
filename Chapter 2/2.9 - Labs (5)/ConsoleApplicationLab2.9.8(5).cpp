f60c7811b31e1082e418bbeeaf4cdb964272b0e5
denisrogovoy <denis@knu.ua>
2017-12-16 16:34:27 +0200

    Lab2.9.8(5)

---
+++ Chapter 2/2.9 - Labs (5)/ConsoleApplicationLab2.9.8(5).cpp
@@ -0,0 +1,38 @@
+// ConsoleApplicationLab2.9.8(5).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	double matrix[][4] = { { 1, 2, 3, 4 },
+	{ 2, 2, 3, 1 },
+	{ 3, 3, 3, 2 },
+	{ 4, 1, 2, 4 } };
+	int side = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+	bool issymmetric = true;
+	// Insert your code here
+	for (int i = 0; i < side; i++)
+		for (int j = i; j < side; j++)
+		{
+			if (i == j) 
+				continue;
+			else
+				if (matrix[i][j] != matrix[j][i])
+				{
+					issymmetric = false;
+					break;
+				}
+
+			if (!issymmetric)
+				break;
+		}
+
+	if (issymmetric)
+		cout << "The matrix is symmetric" << endl;
+	else
+		cout << "The matrix is not symmetric" << endl;
+	return 0;
+}
