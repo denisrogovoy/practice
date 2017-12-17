9f8278b3787714603f8ac4c1f6f73dd221032676
denisrogovoy <denis@knu.ua>
2017-12-17 15:30:32 +0200

    Lab4.1.9(1)

---
+++ Chapter 4/4.1 - Labs (1)/ConsoleApplicationLab4.1.9(1).cpp
@@ -0,0 +1,82 @@
+// ConsoleApplicationLab4.1.9(1).cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int GetSubjectsCount()
+{
+	int subjectsCount;
+	do {
+		cout << "Enter subjects count: ";
+		cin >> subjectsCount;
+	} while (subjectsCount <= 0);
+	return subjectsCount;
+}
+
+int **GetGrades(int subjectCount)
+{
+	int** grades = new int*[subjectCount];
+	int gradesCountInCourse;
+	for (int i = 0; i < subjectCount; i++)
+	{
+		do
+		{
+			cout << "Enter count of grades and grades: ";
+			cin >> gradesCountInCourse;
+		} while (gradesCountInCourse > 5 || gradesCountInCourse < 1);
+		grades[i] = new int[gradesCountInCourse + 1];
+		for (int j = 0; j < gradesCountInCourse; j++) {
+			do
+			{
+				cin >> grades[i][j];
+			} while (grades[i][j] <= 0 || grades[i][j] >= 6);
+			grades[i][gradesCountInCourse] = -1;
+		}
+	}
+	return grades;
+}
+
+double CalculateSubjectFinal(int *grades)
+{
+	double gradesSum = 0;
+	int gradesCount = 0;
+	for (int j = 0; ; j++)
+	{
+		if (grades[j] == -1)
+			break;
+		gradesCount++;
+		gradesSum += grades[j];
+	}
+	return gradesSum / gradesCount;
+}
+
+void ShowFinal(int **grades, int subjectsCount)
+{
+	double currentSubjectFinal, subjectsFinalSum = 0;
+	for (int i = 0; i < subjectsCount; i++) {
+		currentSubjectFinal = CalculateSubjectFinal(grades[i]);
+		subjectsFinalSum += currentSubjectFinal;
+		cout.precision(2);
+		cout << fixed << "Course " << i + 1 << ": final " << currentSubjectFinal << ", grades: ";
+		for (int j = 0; ; j++)
+		{
+			if (grades[i][j] == -1)
+				break;
+			cout << grades[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	cout << "Overall final: " << subjectsFinalSum / subjectsCount << endl;
+}
+
+int main()
+{
+	int subjectsCount = GetSubjectsCount();
+	int **grades = GetGrades(subjectsCount);
+	ShowFinal(grades, subjectsCount);
+	return 0;
+}
